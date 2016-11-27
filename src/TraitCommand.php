6ad38a20e0b3c08dd43c0460227a4053bea9f682
Rukhsar Manzoor <rukhsar.man@gmail.com>
2016-11-27 14:55:06 +0000

    trait command updated

--- src/TraitCommand.php
+++ src/TraitCommand.php
@@ -11,7 +11,9 @@ class TraitCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'make:trait';
+    protected $signature = 'make:trait
+                           {TraitName : the name of the trait}
+                           {TraitFolder? : the location of the trait}';
 
     /**
      * The console command description.
@@ -19,6 +21,16 @@ class TraitCommand extends Command
      * @var string
      */
     protected $description = 'A laravel artisan command to generate trait quickly.';
+
+
+    public $inputs = [];
+
+    public $folderPath;
+
+    public $newFilePath;
+
+    public $newFileName;
+
 
     /**
      * Create a new command instance.
@@ -37,6 +49,72 @@ class TraitCommand extends Command
      */
     public function handle()
     {
-        //
+        $this->setInputsFromArtisan();
+
+        if ( $this->makeTraitDirectory()->makeTraitFile() )
+        {
+
+                $this->sendSuccessMessage();
+
+            return;
+       }
+
+        $this->error('Oops, something went wrong during creating command!');
     }
+
+    private function makeTraitDirectory()
+    {
+        if ( !file_exists(base_path($this->folderPath)))
+        {
+            mkdir(base_path($this->folderPath));
+        }
+        return $this;
+    }
+
+    private function makeTraitFile()
+    {
+        $txt = $this->traitStub();
+
+        $handle = fopen($this->newFilePath, "w");
+
+        fwrite($handle, $txt);
+
+        fclose($handle);
+
+        return $this;
+
+    }
+
+    private function setInputsFromArtisan()
+    {
+        // sets inputs from the artisan command line arguments
+        $this->inputs = $this->argument();
+
+        $this->folderPath = 'app/' . $this->inputs['TraitFolder'];
+
+        $this->newFileName = $this->inputs['TraitName'] . '.php';
+
+        $this-> newFilePath = base_path($this->folderPath) . '/' . $this->inputs['TraitName'] . '.php';
+
+    }
+
+    private function traitStub()
+    {
+        $phpOpenTag = "<?php\n\n";
+
+        $comment = "// Trait created using make:trait command \n\n";
+
+        $nameSpace = "namespace App\\" . $this->inputs['TraitFolder'] . ';' . "\n\n";
+
+        $traitDeclaration = "trait " . $this->inputs['TraitName'] . "\n" . "{\n\n}";
+
+        return $phpOpenTag . $comment .$nameSpace . $traitDeclaration;
+
+    }
+
+    private function sendSuccessMessage()
+    {
+        $this->info($this->newFileName . ' successfully created');
+    }
+
 }
